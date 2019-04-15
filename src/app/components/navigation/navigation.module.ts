5bf27d364f40f808ea14fb60d4946620dd8fdfe0
Alex Martin <amartin@excilys.com>
2019-04-15 17:32:03 +0200

    [FEATURE] Skeleton of the application

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PageSizePickerComponent } from './page-size-picker/page-size-picker.component';
 import { NavigationWrapperComponent } from './navigation-wrapper/navigation-wrapper.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-  declarations: [SidebarComponent, PageSizePickerComponent, NavigationWrapperComponent],
+  declarations: [SidebarComponent, PageSizePickerComponent, NavigationWrapperComponent, TopbarComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent, PageSizePickerComponent
+    SidebarComponent, PageSizePickerComponent, NavigationWrapperComponent, TopbarComponent
   ]
 })
 export class NavigationModule { }
