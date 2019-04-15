f0bceb03cbf081ebd8ef062031446bcdf90ec7c4
chrwizi <superferi93@gmail.com>
2019-04-15 17:38:06 +0200

    [update] [routing] mise en place du routage sauf pour le edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {buildPath} from 'selenium-webdriver/http';
 
-const routes: Routes = [];
+import {ComputerDashboardModule} from './components/computer-dashboard/computer-dashboard.module';
+import {ComputerAddFormComponent} from './components/computer-dashboard/computer-add-form/computer-add-form.component';
+import {ComputerDeleteFormComponent} from './components/computer-dashboard/computer-delete-form/computer-delete-form.component';
+import {ComputerTableComponent} from './components/computer-dashboard/computer-table/computer-table.component';
+
+const routes: Routes = [
+
+  {
+    path: 'computers',
+    component: ComputerTableComponent
+  },
+
+  {
+    path: 'computers/add',
+    component: ComputerAddFormComponent
+  },
+
+  {
+    path: 'computers/delete/:id',
+    component: ComputerDeleteFormComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'computers',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
