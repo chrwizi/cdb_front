423d9934407955ffaca7cdd69603633a900bc339
Valentin Rousseau <valentin.rousseau02@gmail.com>
2019-04-16 15:12:56 +0200

    [ADD] Material table and nav templates

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavigationWrapperComponent } from './navigation-wrapper/navigation-wrapper.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { CustomMaterialModule } from 'src/app/custom-material/custom-material.module';
 
 @NgModule({
   declarations: [SidebarComponent, NavigationWrapperComponent, TopbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CustomMaterialModule
   ],
   exports: [
     SidebarComponent, NavigationWrapperComponent, TopbarComponent
