168b309081c62c2251328731ed3e990ca6dd92b2
Marc <mqu@excilys.com>
2019-04-16 11:01:00 +0200

    [wip] services and table/row components

--- src/app/components/company-dashboard/company-table/company-table.component.ts
+++ src/app/components/company-dashboard/company-table/company-table.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Company } from 'src/app/models/company.model';
+import { CompanyService } from 'src/app/services/company/company.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-company-table',
   templateUrl: './company-table.component.html',
   styleUrls: ['./company-table.component.scss']
 })
-export class CompanyTableComponent implements OnInit {
 
-  constructor() { }
+export class CompanyTableComponent {
+  companies: Company[];
 
-  ngOnInit() {
+  constructor(private companyService: CompanyService,  private router : ActivatedRoute){
   }
 
+  ngOnInit() {
+    this.companyService.getCompanies().subscribe(
+      companies => this.companies = companies
+    );
+  }
 }
--- src/app/components/computer-dashboard/computer-row/computer-row.component.ts
+++ src/app/components/computer-dashboard/computer-row/computer-row.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Computer } from 'src/app/models/computer.model';
 
 @Component({
   selector: 'app-computer-row',
   templateUrl: './computer-row.component.html',
   styleUrls: ['./computer-row.component.scss']
 })
-export class ComputerRowComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ComputerRowComponent {
+  @Input()
+  computer: Computer;
 }
