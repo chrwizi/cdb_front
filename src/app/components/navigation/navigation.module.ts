dc21cd61429eb8b22960e805046500b3a76820f2
chrwizi <superferi93@gmail.com>
2019-04-16 14:48:16 +0200

    [wip]

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { PageSizePickerComponent } from './page-size-picker/page-size-picker.component';
 import { NavigationWrapperComponent } from './navigation-wrapper/navigation-wrapper.component';
 import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-  declarations: [SidebarComponent, PageSizePickerComponent, NavigationWrapperComponent, TopbarComponent],
+  declarations: [SidebarComponent, NavigationWrapperComponent, TopbarComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent, PageSizePickerComponent, NavigationWrapperComponent, TopbarComponent
+    SidebarComponent, NavigationWrapperComponent, TopbarComponent
   ]
 })
 export class NavigationModule { }
