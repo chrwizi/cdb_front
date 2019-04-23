794d78a2177d1c76d30d3ec6857ecd1d3a8eaa00
Valentin Rousseau <valentin.rousseau02@gmail.com>
2019-04-23 16:57:43 +0200

    [DEV] added sidebar nav

--- src/app/components/navigation/sidebar/sidebar.component.ts
+++ src/app/components/navigation/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onComputersList():void{
+    this.router.navigate(['computers/']);
+  }
+
+  onCompaniesList():void{
+    this.router.navigate(['companies/']);
   }
 
 }
