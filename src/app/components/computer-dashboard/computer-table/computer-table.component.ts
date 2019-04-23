ca3848aed19ef6dfe1ee81e31ef6b61bed40f61c
Valentin Rousseau <valentin.rousseau02@gmail.com>
2019-04-23 12:18:03 +0200

    [DEV] Pagination (material side based on getall) for computers

--- src/app/components/computer-dashboard/computer-table/computer-table.component.ts
+++ src/app/components/computer-dashboard/computer-table/computer-table.component.ts
@@ -1,7 +1,7 @@
 import { Computer } from 'src/app/models/computer.model';
 import { ComputerService } from 'src/app/services/computer/computer.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-computer-table',
@@ -11,18 +11,25 @@ import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 export class ComputerTableComponent implements OnInit {
   computers: Computer[];
+  dataSource = new MatTableDataSource<Computer>(this.computers);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  
   displayedColumns: string[] = ['name', 'introduced', 'discontinued', 'company'];
 
 
   constructor(private computerService: ComputerService){
-    //this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
-    //this.dataSource.paginator = this.paginator;
-    this.computerService.getComputers().subscribe( computers => this.computers = computers );
+    this.computerService.getComputers().subscribe( computers => {
+      this.computers = computers
+      this.dataSource = new MatTableDataSource<Computer>(this.computers);
+      this.dataSource.paginator = this.paginator;
+    }  );
+    
   }
 
 }
