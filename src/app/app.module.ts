7b8dd21c24f174eeb93589c9ced01165f16863f3
Valentin Rousseau <valentin.rousseau02@gmail.com>
2019-04-26 13:31:51 +0200

    [FIX] Language buttons not appearing when not logged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,6 @@ import { LoggingService } from './services/logging/logging.service';
   declarations: [
     AppComponent,
     UserLoginComponent,
-    GlobalWrapperComponent,
     UserRegisterComponent,
     UserLoginComponent
   ],
